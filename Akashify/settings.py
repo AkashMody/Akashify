e9a45bc489ecbfff0c0d17545d5665e48ccdc57e
AkashMody <48692059+AkashMody@users.noreply.github.com>
2024-06-03 10:44:26 +0530

    Update settings.py

--- Akashify/settings.py
+++ Akashify/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "mody.akash0808@gmail.com"
-EMAIL_HOST_PASSWORD = "hwoxikqasltjrxgr"
+EMAIL_HOST_PASSWORD = "*****************"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
